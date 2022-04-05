9fc28724dfb58af7d55e61d398538f4003b52c98
unknown <asthaagarwal203@gmail.com>
2022-04-05 22:00:23 +0530

    [Astha] Added: UC5 Calulate wages for a month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,17 +4,39 @@ const PART_TIME_HOUR = 4;
 const PRESENT = 1;
 const ABSENT = 0;
 const PARTIAL_PRESENT = 2;
-let empCheck=Math.floor(Math.random()*10)%3;
 let totalWage = 0;
-               switch (empCheck)
-            {
-                case PARTIAL_PRESENT:
-                    totalWage = PART_TIME_HOUR * WAGE_PER_HOUR;
-                    break;
-                case ABSENT:
-                    break;
-                case PRESENT:
-                    totalWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                    break;
-            }
-console.log("Daily employee wage is " + totalWage) ;
+let totalWorkingHours=0;
+let empWage=0;
+let day=0;
+while(day<20)
+{
+    let empCheck=Math.floor(Math.random()*10)%3;
+          totalWorkingHours=totalWorkingHours+getWorkingHr(empCheck);
+          empWage=CalEmpWage(totalWorkingHours);
+            day++;
+        }
+console.log("Total working hr is " + totalWorkingHours) ;
+console.log("No of Days is "+day);
+console.log("Employee Wage is "+empWage);
+
+function getWorkingHr(empCheck)
+{
+    let empHr=0;
+    switch (empCheck)
+    {
+        case PARTIAL_PRESENT:
+            empHr= PART_TIME_HOUR;
+            break;
+        case ABSENT:
+            empHr=0;
+            break;
+        case PRESENT:
+            empHr = FULL_DAY_HOUR;
+            break;
+    }
+    return empHr;
+}
+function CalEmpWage(workingHr)
+{
+return workingHr*WAGE_PER_HOUR;
+}
