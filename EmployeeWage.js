f800ef517a382acfdc315b1311da67487e2c0193
unknown <asthaagarwal203@gmail.com>
2022-04-05 21:47:57 +0530

    [Astha] Added: UC4 Using Switch Case Statement

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,7 +1,20 @@
 const WAGE_PER_HOUR = 20;
 const FULL_DAY_HOUR = 8;
-const PART_TIME_HOUR = 8;
-const dailyEmployeeWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-const partTimeEmployeeWage = PART_TIME_HOUR * WAGE_PER_HOUR;
- console.log("Daily employee wage is "+dailyEmployeeWage);
- console.log("Part time employee wage is " + partTimeEmployeeWage);
+const PART_TIME_HOUR = 4;
+const PRESENT = 1;
+const ABSENT = 0;
+const PARTIAL_PRESENT = 2;
+let empCheck=Math.floor(Math.random()*10)%3;
+let totalWage = 0;
+               switch (empCheck)
+            {
+                case PARTIAL_PRESENT:
+                    totalWage = PART_TIME_HOUR * WAGE_PER_HOUR;
+                    break;
+                case ABSENT:
+                    break;
+                case PRESENT:
+                    totalWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    break;
+            }
+console.log("Daily employee wage is " + totalWage) ;
