290242cda970b50df3d6c73780adfe7d01139b00
unknown <asthaagarwal203@gmail.com>
2022-04-05 21:43:25 +0530

    [Astha] Added: Add part time hr in employee

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,4 +1,7 @@
 const WAGE_PER_HOUR = 20;
 const FULL_DAY_HOUR = 8;
+const PART_TIME_HOUR = 8;
 const dailyEmployeeWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-console.log("Daily employee wage is "+dailyEmployeeWage);
+const partTimeEmployeeWage = PART_TIME_HOUR * WAGE_PER_HOUR;
+ console.log("Daily employee wage is "+dailyEmployeeWage);
+ console.log("Part time employee wage is " + partTimeEmployeeWage);
