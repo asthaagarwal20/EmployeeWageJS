be3f3a4d1f6e9f8e7404846b728d3cc4d5c8e6f8
unknown <asthaagarwal203@gmail.com>
2022-04-07 16:12:54 +0530

    [Astha] Added: UC9_Calculate total working hrs using arrow function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -10,10 +10,12 @@ let empWage=0;
 let day=0;
 let dailyEmpWage=new Array();
 let empWageMap=new Map();
+let empDailyHrsMap=new Map();
 while(day<20&&totalWorkingHours<160)
 {
     let empCheck=Math.floor(Math.random()*10)%3;
     let empWorkingHr=getWorkingHr(empCheck);
+    empDailyHrsMap.set(day+1,empWorkingHr);
           totalWorkingHours=totalWorkingHours+empWorkingHr;
           dailyEmpWage.push(CalEmpWage(empWorkingHr));
           empWageMap.set(day+1,CalEmpWage(empWorkingHr));
@@ -47,8 +49,28 @@ function CalEmpWage(workingHr)
 {
 return workingHr*WAGE_PER_HOUR;
 }
- function totalEmpWage(totalWage,dailyWage)
- {
-     return totalWage+dailyWage;
- }
- console.log("total wage is "+Array.from(empWageMap.values()).reduce(totalEmpWage,0));
+const findTotal=(totalValue,dailyValue)=>{
+    return dailyValue+totalValue;
+}
+let totalHours=Array.from(empDailyHrsMap.values()).reduce(findTotal,0);
+let totalSalary=dailyEmpWage.filter(dailyWage=>dailyWage>0).reduce(findTotal,0);
+
+console.log("Total employee Wage is "+totalSalary);
+console.log("Total employee working hr is "+totalHours);
+
+let nonWorkingDays=new Array();
+let PartTimeWorkingDays=new Array();
+let FullWorkingDays=new Array();
+
+empDailyHrsMap.forEach((value,key,map)=>
+{
+    if(value==8)
+    FullWorkingDays.push(key);
+    else if(value==4)
+    PartTimeWorkingDays.push(key);
+    else
+    nonWorkingDays.push(key);
+})
+console.log("full working days:"+FullWorkingDays);
+console.log("part time working days"+PartTimeWorkingDays);
+console.log("non working days"+nonWorkingDays);
