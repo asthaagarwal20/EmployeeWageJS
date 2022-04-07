6fcb901b5469222288840d0f7a31eef31c5777c1
unknown <asthaagarwal203@gmail.com>
2022-04-07 16:48:36 +0530

    [Astha] Added: UC10 Ability to store day,hours worked and wage earned in single object

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -9,18 +9,29 @@ let totalWorkingHours=0;
 let empWage=0;
 let day=0;
 let dailyEmpWage=new Array();
+let dailyEmpWageAndDailyHrs=new Array();
 let empWageMap=new Map();
 let empDailyHrsMap=new Map();
 while(day<20&&totalWorkingHours<160)
 {
     let empCheck=Math.floor(Math.random()*10)%3;
     let empWorkingHr=getWorkingHr(empCheck);
-    empDailyHrsMap.set(day+1,empWorkingHr);
           totalWorkingHours=totalWorkingHours+empWorkingHr;
           dailyEmpWage.push(CalEmpWage(empWorkingHr));
           empWageMap.set(day+1,CalEmpWage(empWorkingHr));
             day++;
-        }
+            dailyEmpWageAndDailyHrs.push(
+                {
+                    dayNum:day,
+                    dailyhours:empWorkingHr,
+                    dailyWage:CalEmpWage(empWorkingHr),
+                    toString(){
+                        return '\nday'+this.dayNum+' dailyHours '+this.dailyhours+' dailywage '+this.dailyWage                
+                    },
+                }
+            );
+}
+console.log("UC10"+dailyEmpWageAndDailyHrs);
         totalWage=CalEmpWage(totalWorkingHours);
 console.log("Total working hr is " + totalWorkingHours) ;
 console.log("No of Days is "+day);
@@ -49,28 +60,4 @@ function CalEmpWage(workingHr)
 {
 return workingHr*WAGE_PER_HOUR;
 }
-const findTotal=(totalValue,dailyValue)=>{
-    return dailyValue+totalValue;
-}
-let totalHours=Array.from(empDailyHrsMap.values()).reduce(findTotal,0);
-let totalSalary=dailyEmpWage.filter(dailyWage=>dailyWage>0).reduce(findTotal,0);
-
-console.log("Total employee Wage is "+totalSalary);
-console.log("Total employee working hr is "+totalHours);
-
-let nonWorkingDays=new Array();
-let PartTimeWorkingDays=new Array();
-let FullWorkingDays=new Array();
-
-empDailyHrsMap.forEach((value,key,map)=>
-{
-    if(value==8)
-    FullWorkingDays.push(key);
-    else if(value==4)
-    PartTimeWorkingDays.push(key);
-    else
-    nonWorkingDays.push(key);
-})
-console.log("full working days:"+FullWorkingDays);
-console.log("part time working days"+PartTimeWorkingDays);
-console.log("non working days"+nonWorkingDays);
+ 
